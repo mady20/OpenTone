7fc467d0e76c0dae23e3901973e543a00b75ee7a
Harsh1841 <harsh1841.be23@chitkara.edu.in>
2025-11-26 19:52:52 +0530

    fix : roleplays screen

--- OpenTone/RoleplayCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RoleplayCell.swift
-//  OpenTone
-//
-//  Created by Student on 25/11/25.
-//
-
-import Foundation
---
+++ OpenTone/RoleplaysCell.swift
@@ -0,0 +1,12 @@
+//
+//  RoleplaysCell.swift
+//  OpenTone
+//
+//  Created by Harshdeep Singh on 26/11/25.
+//
+
+import UIKit
+
+class RoleplaysCell: UICollectionViewCell {
+    
+}
