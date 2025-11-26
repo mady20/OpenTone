ca96ccf06ed02e8f7b32bc2f16be3663f893d157
mady020 <219156334+mady020@users.noreply.github.com>
2025-11-26 10:04:19 +0530

    Feat/login (#4)
    * bg_done
    
    added elements and bg and liquid srystal
    
    * login
    
    settng screen
    
    * login and sign up page
    
    todo set the heading
    
    * next step
    
    started work for data passing
    
    * outlets
    
    * started Jam Session
    
    jam session screen 1
    
    * JAM screen1
    
    Completed
    
    ---------
    
    Co-authored-by: Keerti <keerti0460.be23@chitkara.edu.in>

---
+++ OpenTone/LoginView.swift
@@ -0,0 +1,36 @@
+//
+//  LoginView.swift
+//  OpenTone
+//
+//  Created by Student on 24/11/25.
+//
+
+import UIKit
+
+import Foundation
+
+class LoginViewController: UIViewController {
+    
+    @IBAction func SignInButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func ContinueWIthAppleButtonTapped(_ sender: Any) {
+    }
+
+    @IBAction func U_forImageTapped(_ sender: Any) {
+    }
+    
+    @IBOutlet var usernameField: UITextField!
+    
+    @IBOutlet var passwordField: UITextField!
+
+    @IBAction func forgotPasswordClick(_ sender: Any) {
+    }
+    
+    @IBAction func HaveAaccSignInClicked(_ sender: Any) {
+    }
+    
+    
+    
+
+}
