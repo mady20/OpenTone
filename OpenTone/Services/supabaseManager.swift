12298db9b38bfab278bfd73fcb8428535d24675d
harshdeep1841 <harshdeepsingh10d@gmail.com>
2026-02-23 10:36:27 +0530

    added supabase dependency

---
+++ OpenTone/Services/supabaseManager.swift
@@ -0,0 +1,16 @@
+//
+//  supabaseManager.swift
+//  OpenTone
+//
+//  Created by Harshdeep Singh on 23/02/26.
+//
+
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://tnjawcfajxmcnucsfrzm.supabase.co")!,
+  supabaseKey: "sb_publishable_2BgOnfn_woDQlWE5KY8saA_pCYzsKO1"
+)
+        
